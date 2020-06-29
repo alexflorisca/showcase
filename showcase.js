ca13341be3ac8b53d0a0ae6514bcbbe27427b765
Alex Florisca <alexflorisca@gmail.com>
2020-06-29 17:09:36 +0100

    Accessibility improvements

--- showcase.js
+++ showcase.js
@@ -49,6 +49,7 @@ class Showcase {
 		this.slides.forEach((slide, index) => {
 			const dot = document.createElement('button');
 			dot.setAttribute('data-slide-index', index)
+			dot.setAttribute('aria-label', `Image ${index+1} of ${this.slides.length}`)					
 			dot.classList.add('showcase__dot');
 			if(index === this.activeSlideIndex) {
 				dot.classList.add('active');
